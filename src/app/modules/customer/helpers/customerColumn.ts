128bb588567fe17a5931463df42328d849799961
clonerops <114849678+clonerops@users.noreply.github.com>
2023-09-28 10:59:55 -0700

    customer complete1

--- src/app/modules/customer/helpers/customerColumn.ts
+++ src/app/modules/customer/helpers/customerColumn.ts
@@ -26,21 +26,25 @@ export const columns = (renderAction: any) => {
   const col = [
     { field: 'id', headerName: 'کد مشتری', headerClassName: "bg-[#2E4374] text-white" },
     { field: 'firstName', headerName: 'نام', headerClassName: "bg-[#2E4374] text-white" },
-    { field: 'lastName', headerName: 'نام خانوادگی', headerClassName: "bg-[#2E4374] text-white" },
+    { field: 'lastName', headerName: 'نام خانوادگی', headerClassName: "bg-[#2E4374] text-white", width: 150 },
     { field: 'nationalId', headerName: 'کدملی', headerClassName: "bg-[#2E4374] text-white" },
     {
-      field: 'customerType', headerName: 'نوع مشتری', renderCell: (params: any) => (
+      field: 'customerType', headerName: 'نوع مشتری', width: 80, renderCell: (params: any) => (
         params.value === 0 ? "حقیقی" : "حقوقی"
       ), headerClassName: "bg-[#2E4374] text-white"
     },
-    { field: 'customerValidityId', headerName: 'نوع اعتبار',
-    renderCell: (params: any) => (
-      params.value === 1 ? "عادی" : params.value === 2 ? "VIP" : "سیاه"
-    ), headerClassName: "bg-[#2E4374] text-white" },
+    {
+      field: 'customerValidityId', headerName: 'نوع اعتبار', width: 80,
+      renderCell: (params: any) => (
+        params.value === 1 ? "عادی" : params.value === 2 ? "VIP" : "سیاه"
+      ), headerClassName: "bg-[#2E4374] text-white"
+    },
     { field: 'mobile', headerName: 'موبایل', headerClassName: "bg-[#2E4374] text-white" },
-    { field: 'isSupplier', headerName: 'تامین کننده؟', headerClassName: "bg-[#2E4374] text-white" },
-    { field: 'address1', headerName: 'آدرس یک', headerClassName: "bg-[#2E4374] text-white" },
-    { field: 'address2', headerName: 'آدرس دو', headerClassName: "bg-[#2E4374] text-white" },
+    { field: 'isSupplier', headerName: 'تامین کننده؟', renderCell: (params: any) => (
+      params.value === true ? "بله" : "خیر"
+    ), headerClassName: "bg-[#2E4374] text-white", cellClassName: "text-center", width: 80 },
+    { field: 'address1', headerName: 'آدرس یک', headerClassName: "bg-[#2E4374] text-white", width: 280 },
+    { field: 'address2', headerName: 'آدرس دو', headerClassName: "bg-[#2E4374] text-white", width: 280 },
     { field: 'representative', headerName: 'معرف', headerClassName: "bg-[#2E4374] text-white" },
     { headerName: 'عملیات', renderCell: renderAction, headerClassName: "bg-[#2E4374] text-white" }
   ]
