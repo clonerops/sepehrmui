8101985915f61f0bd5772cc7d460f6f8bcf5804b
Cloner ops <clonerops@gmail.com>
2023-10-19 11:37:08 +0330

    payment change grid ui

--- src/app/modules/payment/PaymentAccounting.tsx
+++ src/app/modules/payment/PaymentAccounting.tsx
@@ -1,20 +1,19 @@
-import { useState, useEffect } from "react"
-import { useGetRecievePaymentByApproved } from "./core/_hooks"
-import { Link } from "react-router-dom"
-import Backdrop from "../../../_cloner/components/Backdrop"
-import { Box, Button, Card, Container, Typography } from "@mui/material"
-import MuiDataGrid from "../../../_cloner/components/MuiDataGrid"
-import FuzzySearch from "../../../_cloner/helpers/Fuse"
-import { IPayment } from "./core/_models"
-import React from "react"
-import { separateAmountWithCommas } from "../../../_cloner/helpers/SeprateAmount"
-import { Visibility } from "@mui/icons-material"
-import ActiveText from "../../../_cloner/components/ActiveText"
-
+import { useState, useEffect } from "react";
+import { useGetRecievePaymentByApproved } from "./core/_hooks";
+import { Link } from "react-router-dom";
+import Backdrop from "../../../_cloner/components/Backdrop";
+import { Box, Button, Card, Container, Typography } from "@mui/material";
+import MuiDataGrid from "../../../_cloner/components/MuiDataGrid";
+import FuzzySearch from "../../../_cloner/helpers/Fuse";
+import { IPayment } from "./core/_models";
+import React from "react";
+import { separateAmountWithCommas } from "../../../_cloner/helpers/SeprateAmount";
+import { Visibility } from "@mui/icons-material";
+import ActiveText from "../../../_cloner/components/ActiveText";
 
 const PaymentAccounting = () => {
     // Fetching
-    const { mutate, data, isLoading } = useGetRecievePaymentByApproved()
+    const { mutate, data, isLoading } = useGetRecievePaymentByApproved();
     // States
     const [results, setResults] = useState<IPayment[]>([]);
 
@@ -22,80 +21,152 @@ const PaymentAccounting = () => {
         setResults(data?.data);
     }, [data?.data]);
     useEffect(() => {
-        mutate("0")
+        mutate("0");
         // eslint-disable-next-line
-    }, [])
+    }, []);
 
     const columns = (renderAction: any) => {
         const col = [
             {
-                field: 'receivePayCode', renderCell: (params: any) => {
-                    return <Typography variant="h3">{params.value}</Typography >;
-                }, headerName: 'شماره ثبت', headerClassName: "headerClassName", minWidth: 80
+                field: "receivePayCode",
+                renderCell: (params: any) => {
+                    return <Typography variant="h4">{params.value}</Typography>;
+                },
+                headerName: "شماره ثبت",
+                headerClassName: "headerClassName",
+                minWidth: 80,
+                flex: 1
             },
             {
-                field: 'receivePaymentSourceFromDesc', headerName: 'دریافت از',
+                field: "receivePaymentSourceFromDesc",
+                headerName: "دریافت از",
                 renderCell: (value: any) => (
-                    <Typography variant="h3">{value.row.receivePaymentSourceFromDesc + " " + (value.row?.receiveFromCustomerName === null ? "" : value.row?.receiveFromCustomerName)}</Typography >
+                    <Typography variant="h4">
+                        {value.row.receivePaymentSourceFromDesc +
+                            " " +
+                            (value.row?.receiveFromCustomerName === null
+                                ? ""
+                                : value.row?.receiveFromCustomerName)}
+                    </Typography>
                 ),
-                headerClassName: "headerClassName", minWidth: 240
+                headerClassName: "headerClassName",
+                minWidth: 240,
+                flex: 1
             },
             {
-                field: 'receivePaymentSourceToDesc',
+                field: "receivePaymentSourceToDesc",
                 renderCell: (value: any) => (
-                    <Typography variant="h3">{value.row.receivePaymentSourceToDesc + " " + (value.row?.payToCustomerName === null ? "" : value.row?.payToCustomerName)}</Typography >
-                ), headerName: 'پرداخت به', headerClassName: "headerClassName", minWidth: 240
-            },
-            {
-                field: 'amount', headerName: 'مبلغ',
-                renderCell: (value: any) => (
-                    <Typography variant="h3">{separateAmountWithCommas(value.row.amount)}</Typography >
+                    <Typography variant="h4">
+                        {value.row.receivePaymentSourceToDesc +
+                            " " +
+                            (value.row?.payToCustomerName === null
+                                ? ""
+                                : value.row?.payToCustomerName)}
+                    </Typography>
                 ),
-                headerClassName: "headerClassName", minWidth: 160
+                headerName: "پرداخت به",
+                headerClassName: "headerClassName",
+                minWidth: 240,
+                flex: 1
             },
             {
-                field: 'accountOwner', renderCell: (params: any) => {
-                    return <Typography variant="h3">{params.value}</Typography >;
-                }, headerName: 'صاحب حساب', headerClassName: "headerClassName", minWidth: 180
+                field: "amount",
+                headerName: "مبلغ",
+                renderCell: (value: any) => (
+                    <Typography color="secondary" variant="h4">
+                        {separateAmountWithCommas(value.row.amount)+ "تومان"}
+                    </Typography>
+                ),
+                headerClassName: "headerClassName",
+                minWidth: 160,
+                flex: 1
             },
             {
-                field: 'trachingCode', renderCell: (params: any) => {
-                    return <Typography variant="h3">{params.value}</Typography >;
-                }, headerName: 'کد پیگیری', headerClassName: "headerClassName", minWidth: 180
+                field: "accountOwner",
+                renderCell: (params: any) => {
+                    return <Typography variant="h5">{params.value}</Typography>;
+                },
+                headerName: "صاحب حساب",
+                headerClassName: "headerClassName",
+                minWidth: 120,
+                flex: 1
             },
             {
-                field: 'companyName', renderCell: (params: any) => {
-                    return <Typography variant="h3">{params.value}</Typography >;
-                }, headerName: 'صاحب شرکت', headerClassName: "headerClassName", minWidth: 100
+                field: "trachingCode",
+                renderCell: (params: any) => {
+                    return <Typography variant="h5">{params.value}</Typography>;
+                },
+                headerName: "کد پیگیری",
+                headerClassName: "headerClassName",
+                minWidth: 120,
+                flex: 1
             },
             {
-                field: 'contractCode', renderCell: (params: any) => {
-                    return <Typography variant="h3">{params.value}</Typography >;
-                }, headerName: 'شماره قرارداد', headerClassName: "headerClassName", minWidth: 100
+                field: "companyName",
+                renderCell: (params: any) => {
+                    return <Typography variant="h5">{params.value}</Typography>;
+                },
+                headerName: "صاحب شرکت",
+                headerClassName: "headerClassName",
+                minWidth: 100,
+                flex: 1
             },
             {
-                field: 'isAccountingApproval', headerName: 'تایید حسابداری؟', renderCell: (params: any) => {
-                    return <ActiveText params={params} successTitle="بله" dangerTitle="خیر" />
-                }, headerClassName: "headerClassName", minWidth: 100
+                field: "contractCode",
+                renderCell: (params: any) => {
+                    return <Typography variant="h4">{params.value}</Typography>;
+                },
+                headerName: "شماره قرارداد",
+                headerClassName: "headerClassName",
+                minWidth: 100,
+                flex: 1
             },
             {
-                field: 'accountingApprovalDate', renderCell: (params: any) => {
-                    return <Typography variant="h3">{params.value}</Typography >;
-                }, headerName: 'تاریخ تایید حسابداری', headerClassName: "headerClassName", minWidth: 160
+                field: "isAccountingApproval",
+                headerName: "تایید حسابداری؟",
+                renderCell: (params: any) => {
+                    return (
+                        <ActiveText
+                            params={params}
+                            successTitle="بله"
+                            dangerTitle="خیر"
+                        />
+                    );
+                },
+                headerClassName: "headerClassName",
+                minWidth: 100,
+                flex: 1
             },
-            { headerName: 'جزئیات و تایید حسابداری', renderCell: renderAction, headerClassName: "headerClassName", minWidth: 260 }
-        ]
-        return col
-    }
+            {
+                field: "accountingApprovalDate",
+                renderCell: (params: any) => {
+                    return <Typography variant="h4">{params.value}</Typography>;
+                },
+                headerName: "تاریخ تایید حسابداری",
+                headerClassName: "headerClassName",
+                minWidth: 160,
+                flex: 1
+            },
+            {
+                headerName: "جزئیات",
+                renderCell: renderAction,
+                headerClassName: "headerClassName",
+                minWidth: 80,
+                flex: 1
+            },
+        ];
+        return col;
+    };
 
     const renderActions = (item: any) => {
-        return <Link to={`/dashboard/payment/accounting/${item?.row?.id}`}>
-            <Typography variant="h3">
-                <Visibility color="secondary" />
-            </Typography >
-        </Link>
-
-    }
+        return (
+            <Link to={`/dashboard/payment/accounting/${item?.row?.id}`}>
+                <Typography variant="h4">
+                    <Visibility color="secondary" />
+                </Typography>
+            </Link>
+        );
+    };
     return (
         <>
             {isLoading && <Backdrop loading={isLoading} />}
@@ -120,10 +191,14 @@ const PaymentAccounting = () => {
                         setResults={setResults}
                     />
                 </Box>
-                <MuiDataGrid columns={columns(renderActions)} rows={results} data={data?.data} />
+                <MuiDataGrid
+                    columns={columns(renderActions)}
+                    rows={results}
+                    data={data?.data}
+                />
             </Card>
         </>
-    )
-}
+    );
+};
 
-export default PaymentAccounting
+export default PaymentAccounting;
