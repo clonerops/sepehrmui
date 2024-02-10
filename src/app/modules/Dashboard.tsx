80b57fd82939b79483eb158a225507d2a04663de
Cloner ops <clonerops@gmail.com>
2024-02-10 13:53:02 +0330

    change card className dashboard

--- src/app/modules/Dashboard.tsx
+++ src/app/modules/Dashboard.tsx
@@ -9,17 +9,18 @@ import { FC } from "react";
 
 interface ICardInformation {
     title: string
+    cardClassName: string
     value: any
 }
 
-const CardInformation: FC<ICardInformation> = ({ title, value }) => {
+const CardInformation: FC<ICardInformation> = ({ title, value, cardClassName }) => {
     return (
-        <ReusableCard>
+        <ReusableCard cardClassName={cardClassName}>
             <Box component="div" className="flex justify-between items-center space-y-4">
-                <Typography variant="body1">{title}</Typography>
-                <MonetizationOn color="secondary" />
+                <Typography variant="body1" className="text-white">{title}</Typography>
+                <MonetizationOn className="!text-white" />
             </Box>
-            <Typography variant="h2">{value}</Typography>
+            <Typography variant="h2" className="text-white">{value}</Typography>
         </ReusableCard>
 
     )
@@ -36,10 +37,10 @@ const Dashboard = () => {
     return (
         <>
             <Box component="div" className="grid grid-cols-1 md:grid-cols-4 gap-x-8 space-y-4 md:space-y-0 my-4">
-                <CardInformation title="تعداد سفارشات" value={separateAmountWithCommas(25878)} />
-                <CardInformation title="فروش امروز" value={separateAmountWithCommas(43242)} />
-                <CardInformation title="میانگین قیمت" value={separateAmountWithCommas(77754)} />
-                <CardInformation title="درآمد" value={separateAmountWithCommas(77754)} />
+                <CardInformation cardClassName="!bg-[#3322D8]" title="تعداد سفارشات" value={separateAmountWithCommas(25878)} />
+                <CardInformation cardClassName="!bg-[#369BFD]" title="فروش امروز" value={separateAmountWithCommas(43242)} />
+                <CardInformation cardClassName="!bg-[#F8B30E]" title="میانگین قیمت" value={separateAmountWithCommas(77754)} />
+                <CardInformation cardClassName="!bg-[#EB5553]" title="درآمد" value={separateAmountWithCommas(77754)} />
             </Box>
             <Box component="div" className="grid grid-cols-1 md:grid-cols-2 gap-x-8">
                 <ReusableCard>
