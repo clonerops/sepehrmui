b7727e710083b607dd99393a997563874b978438
clonerops <114849678+clonerops@users.noreply.github.com>
2024-02-23 03:29:15 -0800

    validation product form

--- src/app/modules/generic/products/_validations.ts
+++ src/app/modules/generic/products/_validations.ts
@@ -9,6 +9,28 @@ const createProductValidations = Yup.object().shape({
         .required("افزودن وزن الزامی است"),
     productTypeId: Yup.mixed()
         .required("نوع کالا الزامی است"),
+    numberInPackage: Yup.mixed()
+        .required("تعداد در بسته الزامی است"),
+    productThickness: Yup.mixed()
+        .required("ضخامت الزامی است"),
+    productStandardId: Yup.mixed()
+        .required("استاندارد کالا الزامی است"),
+    productStateId: Yup.mixed()
+        .required("حالت کالا الزامی است"),
+    description: Yup.mixed()
+        .required("توضیحات الزامی است"),
+    productMainUnitId: Yup.mixed()
+        .required("واحد اصلی الزامی است"),
+    productSubUnitId: Yup.mixed()
+        .required("واحد فرعی الزامی است"),
+    exchangeRate: Yup.mixed()
+        .required("نرخ تبدیل الزامی است"),
+    maxInventory: Yup.mixed()
+        .required("حداکثر موجودی الزامی است"),
+    minInventory: Yup.mixed()
+        .required("حداقل موجودی الزامی است"),
+    inventotyCriticalPoint: Yup.mixed()
+        .required("نقطه بحرانی الزامی است"),
 });
 
 export {
