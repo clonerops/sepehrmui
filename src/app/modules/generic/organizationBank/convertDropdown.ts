05bf9bc4719d11e918045525f384ea6741da3634
Cloner ops <clonerops@gmail.com>
2024-04-14 09:52:37 +0330

    change orgnazation dropdown

--- src/app/modules/generic/organizationBank/convertDropdown.ts
+++ src/app/modules/generic/organizationBank/convertDropdown.ts
@@ -2,9 +2,10 @@
 export const dropdownOrganzationBank = (data: any) => {
     return (
         data &&
-        data?.map((obj: { accountOwner: string, accountNo: string, id: string }): any => {
-            const { accountOwner, accountNo,  id } = obj;
-            return { value: id, label: `${accountOwner} - (${accountNo})` };
+        data?.map((obj: { accountOwner: string, accountNo: string, id: string, bank: {bankName: string} }): any => {
+            const { accountOwner, accountNo,  id, bank } = obj;
+            // return { value: id, label: `${accountOwner} - (${accountNo})` };
+            return { value: id, label: `${bank.bankName}- (ص.ح: ${accountOwner})-(ش.ح:${accountNo})` };
         })
     );
 };
