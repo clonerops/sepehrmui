09b2d1931c915141b7c124f0ee5a32f08690073f
clonerops <114849678+clonerops@users.noreply.github.com>
2024-05-10 00:24:27 -0700

    lading list print createdDate

--- src/app/modules/logestic/lading/LadingList.tsx
+++ src/app/modules/logestic/lading/LadingList.tsx
@@ -58,7 +58,7 @@ const LadingList = () => {
             <div className="flex flex-row items-center justify-center gap-x-4">
                 <Tooltip title={<Typography variant='h3'>پرینت</Typography>}>
                     <div className="flex gap-x-4">
-                        <Link to={`/dashboard/ladingPermit_print/${item?.row?.cargoAnnounceId}/${item?.row?.id}/${item?.row?.createDate}`}>
+                        <Link to={`/dashboard/ladingPermit_print/${item?.row?.cargoAnnounceId}/${item?.row?.id}/${item?.row?.createdDate}`}>
                             <Print color="primary" />
                         </Link>
                     </div>
