be8b7b13b237bb6a7758ab541cb22bc07561fa34
clonerops <114849678+clonerops@users.noreply.github.com>
2024-08-19 13:19:21 -0700

    change axios server url

--- src/_cloner/helpers/axiosConfig.ts
+++ src/_cloner/helpers/axiosConfig.ts
@@ -2,52 +2,52 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 export const http = axios.create({
-    baseURL: "https://iraniansepehr.com/api/",
-    // baseURL: "http://192.168.10.125/api/",
+    // baseURL: "https://iraniansepehr.com/api/",
+    baseURL: "http://api.strom.net/api/",
     headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${Cookies.get("token")}`,
     },
 });
 export const httpAuth = axios.create({
-    baseURL: "https://iraniansepehr.com/api/",
-    // baseURL: "http://192.168.10.125/api/",
+    // baseURL: "https://iraniansepehr.com/api/",
+    baseURL: "http://api.strom.net/api/",
     headers: {
         "Content-Type": "application/json",
     },
 });
 export const httpFormData = axios.create({
-    baseURL: "https://iraniansepehr.com/api/",
-    // baseURL: "http://192.168.10.125/api/",
+    // baseURL: "https://iraniansepehr.com/api/",
+    baseURL: "http://api.strom.net/api/",
     headers: {
         "Content-Type": "multipart/form-data",
         Authorization: `Bearer ${Cookies.get("token")}`,
     },
 });
 
-// http.interceptors.response.use(
-//     (response) => response,
-//     async (error) => {
-//         const originalRequest = error.config;
-//         // if(!error.response && error.code === "ERR_NETWORK") {
-//         //     window.location.href = "/dashboard/accessDenied"
-//         // } else if(!error.response && error.request) {
-//         //     Cookies.remove("token");
-//         //     window.location.reload();
+http.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
+        // if(!error.response && error.code === "ERR_NETWORK") {
+        //     window.location.href = "/dashboard/accessDenied"
+        // } else if(!error.response && error.request) {
+        //     Cookies.remove("token");
+        //     window.location.reload();
 
-//         // } 
+        // } 
         
-//         if (!error.response && error.request) {
-//                 Cookies.remove("token");
-//             window.location.reload();
-//         }
+        if (!error.response && error.request) {
+                Cookies.remove("token");
+            window.location.reload();
+        }
 
-//         if (error.respons && !originalRequest._retry) {
-//             originalRequest._retry = true;
-//             Cookies.remove("token");
-//             window.location.reload();
-//         }
+        if (error.respons && !originalRequest._retry) {
+            originalRequest._retry = true;
+            Cookies.remove("token");
+            window.location.reload();
+        }
       
-//         return Promise.reject(error);
-//     }
-// );
+        return Promise.reject(error);
+    }
+);
