1093c1e672af622a9ad3132d7ab8df82c4256318
Masoumi <1014382@saipacorp.com>
2024-09-08 11:49:04 +0330

    change version stimulsoft

--- src/_cloner/components/ReportViewer.tsx
+++ src/_cloner/components/ReportViewer.tsx
@@ -4,12 +4,20 @@ import { Stimulsoft } from 'stimulsoft-reports-js/Scripts/stimulsoft.viewer';
 class ReportViewer extends React.Component {
 	private options: Stimulsoft.Viewer.StiViewerOptions;
 	private viewer: Stimulsoft.Viewer.StiViewer;
-	
+
 	constructor(props: any) {
 		super(props);
 
-		
+
 		this.options = new Stimulsoft.Viewer.StiViewerOptions();
+		Stimulsoft.Base.StiLicense.Key = "6vJhGtLLLz2GNviWmUTrhSqnOItdDwjBylQzQcAOiHkO46nMQvol4ASeg91in+mGJLnn2KMIpg3eSXQSgaFOm15+0l" +
+			"hekKip+wRGMwXsKpHAkTvorOFqnpF9rchcYoxHXtjNDLiDHZGTIWq6D/2q4k/eiJm9fV6FdaJIUbWGS3whFWRLPHWC" +
+			"BsWnalqTdZlP9knjaWclfjmUKf2Ksc5btMD6pmR7ZHQfHXfdgYK7tLR1rqtxYxBzOPq3LIBvd3spkQhKb07LTZQoyQ" +
+			"3vmRSMALmJSS6ovIS59XPS+oSm8wgvuRFqE1im111GROa7Ww3tNJTA45lkbXX+SocdwXvEZyaaq61Uc1dBg+4uFRxv" +
+			"yRWvX5WDmJz1X0VLIbHpcIjdEDJUvVAN7Z+FW5xKsV5ySPs8aegsY9ndn4DmoZ1kWvzUaz+E1mxMbOd3tyaNnmVhPZ" +
+			"eIBILmKJGN0BwnnI5fu6JHMM/9QR2tMO1Z4pIwae4P92gKBrt0MqhvnU1Q6kIaPPuG2XBIvAWykVeH2a9EP6064e11" +
+			"PFCBX4gEpJ3XFD0peE5+ddZh+h495qUc1H2B";
+
 		// Stimulsoft.Base.StiLicense.key = '6vJhGtLLLz2GNviWmUTrhSqnOItdDwjBylQzQcAOiHkgpgFGkUl79uxVs8X+uspx6K+tqdtOB5G1S6PFPRrlVNvMUiSiNYl724EZbrUAWwAYHlGLRbvxMviMExTh2l9xZJ2xc4K1z3ZVudRpQpuDdFq+fe0wKXSKlB6okl0hUd2ikQHfyzsAN8fJltqvGRa5LI8BFkA/f7tffwK6jzW5xYYhHxQpU3hy4fmKo/BSg6yKAoUq3yMZTG6tWeKnWcI6ftCDxEHd30EjMISNn1LCdLN0/4YmedTjM7x+0dMiI2Qif/yI+y8gmdbostOE8S2ZjrpKsgxVv2AAZPdzHEkzYSzx81RHDzZBhKRZc5mwWAmXsWBFRQol9PdSQ8BZYLqvJ4Jzrcrext+t1ZD7HE1RZPLPAqErO9eo+7Zn9Cvu5O73+b9dxhE2sRyAv9Tl1lV2WqMezWRsO55Q3LntawkPq0HvBkd9f8uVuq9zk7VKegetCDLb0wszBAs1mjWzN+ACVHiPVKIk94/QlCkj31dWCg8YTrT5btsKcLibxog7pv1+2e4yocZKWsposmcJbgG0';
 		this.viewer = new Stimulsoft.Viewer.StiViewer(this.options, "StiViewer", false);
 	}
@@ -21,18 +29,18 @@ class ReportViewer extends React.Component {
 			</div>
 		);
 	}
-	
+
 	componentDidMount() {
 		var report = Stimulsoft.Report.StiReport.createNewReport();
 		// report.loadFile(process.env.PUBLIC_URL + 'reports/Report.mrt');
-		report.loadFile(process.env.PUBLIC_URL + 'reports/InvoiceSoftware.mdc');
+		report.loadFile(process.env.PUBLIC_URL + 'reports/customerAccount.mrt');
 
 		this.viewer.report = report;
 		this.viewer.renderHtml("viewerContent");
 	}
 
-	
-    
+
+
 }
 
 export default ReportViewer;
